630b89bdf6c74250126832822cf2d0e848e054b5
bcdanl <bchoe@geneseo.edu>
2024-01-26 19:47:39 -0500

    website-instruction

--- index-target-blank.R
+++ index-target-blank.R
@@ -1,34 +1,30 @@
 system("quarto render")
-# quarto::quarto_render("index.qmd")
-
-# Replace the values
-lec_no <- '01'
-lec_date <- '0124'
-
+quarto::quarto_render("index.qmd") # Must close all index.html if it is opened on a web-browser.
 
 library(stringr)
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
 
 # Read the HTML file
 html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
+
+lec_nos <- c('01')
+lec_dates <- c('0126')
+
+for (i in 1:length(lec_nos)){
+  lec_no <- lec_nos[i]
+  lec_date <- lec_dates[i]
+  
+  old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
+  new_content <- str_c(old_content, " ", 'target="_blank"') 
+  
+  
+  html_as_text <- str_replace_all(html_as_text,
+                                  old_content,
+                                  new_content)
+}
 
 writeLines(html_as_text, 
            "index.html")
 
-
-
-# file.copy("tmp/index.html", "index.html")
-
-# Use system() to execute the rm command
-# system(paste("rm", shQuote("index.html")))
-# Sys.sleep(2)
-# 
-# file.rename("index2.html", "index.html")
