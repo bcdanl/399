2d74d1c875d2eddabe77de21b2f674bfd9b1dede
bcdanl <bchoe@geneseo.edu>
2024-02-02 23:18:33 -0500

    lec2

--- index-target-blank.R
+++ index-target-blank.R
@@ -1,5 +1,5 @@
 system("quarto render")
-quarto::quarto_render("index.qmd") # Must close all index.html if it is opened on a web-browser.
+quarto::quarto_render("index.qmd")
 
 library(stringr)
 
@@ -8,9 +8,9 @@ html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
-
-lec_nos <- c('01')
-lec_dates <- c('0126')
+# Next lecture number & dates
+lec_n <- 2   ;  lec_nos <- str_c('0', as.character(1:lec_n))
+lec_dates <- c('0126', '0202')
 
 for (i in 1:length(lec_nos)){
   lec_no <- lec_nos[i]
@@ -28,3 +28,51 @@ for (i in 1:length(lec_nos)){
 writeLines(html_as_text, 
            "index.html")
 
+
+
+# file.copy("tmp/index.html", "index.html")
+
+# Use system() to execute the rm command
+# system(paste("rm", shQuote("index.html")))
+# Sys.sleep(2)
+# 
+# file.rename("index2.html", "index.html")
+system("quarto render")
+quarto::quarto_render("index.qmd")
+
+library(stringr)
+
+# Read the HTML file
+html_as_text <- readLines("index.html", warn = FALSE)
+system(paste("rm", shQuote("index.html")))
+# Sys.sleep(2)
+
+# Next lecture number & dates
+lec_n <- 4   ;  lec_nos <- str_c('0', as.character(1:lec_n))
+lec_dates <- c('0123', '0125', '0130', '0201')
+
+for (i in 1:length(lec_nos)){
+  lec_no <- lec_nos[i]
+  lec_date <- lec_dates[i]
+  
+  old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
+  new_content <- str_c(old_content, " ", 'target="_blank"') 
+  
+  
+  html_as_text <- str_replace_all(html_as_text,
+                                  old_content,
+                                  new_content)
+}
+
+writeLines(html_as_text, 
+           "index.html")
+
+
+
+# file.copy("tmp/index.html", "index.html")
+
+# Use system() to execute the rm command
+# system(paste("rm", shQuote("index.html")))
+# Sys.sleep(2)
+# 
+# file.rename("index2.html", "index.html")
